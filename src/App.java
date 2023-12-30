b4b0db7442a7145e275718c929a5a2209e56bc81
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-30 20:07:09 -0300

    Expressão lambda declarada

--- src/App.java
+++ src/App.java
@@ -1,26 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
-import util.ProductPredicate;
 
-public class App{
+public class App {
 
-		public static void main(String[] args){
-			
-			List<Product> list = new ArrayList<>();
+    public static void main(String[] args) {
 
-			list.add(new Product("TV", 900.0));
-			list.add(new Product("Mouse", 50.00));
-			list.add(new Product("Tablet" , 350.50));
-			list.add(new Product("HD Case", 80.90));
+        List<Product> list = new ArrayList<>();
 
-			list.removeIf(Product:: nonstaticProductPredicate);
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
 
-				for(Product p :list){
-					System.out.println(p);
-					
-				}		
-		}
-	
-	}
+        double min = 100.0;
+
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
+
+        for (Product p : list) {
+            System.out.println(p);
+
+        }
+    }
+
+}
